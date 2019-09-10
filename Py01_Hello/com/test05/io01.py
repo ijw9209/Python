5e1b692ac12e04374e5a08ba50feb3c193657677
이재웅 <ijw9209@naver.com>
2019-09-10 17:34:16 +0900

    python 190910

---
+++ Py01_Hello/com/test05/io01.py
@@ -0,0 +1,3 @@
+file = open('test01.txt','w')
+file.write('Hello,World!')
+file.close()
---
+++ Py01_Hello/com/test05/io05.py
@@ -0,0 +1,8 @@
+
+import pickle 
+
+score = {'name':'kh' , 'kor':100, 'eng':50 , 'math':44}
+print(type(score))
+
+with open('test02.txt' , 'wb') as file:
+    pickle.dump(score, file)
